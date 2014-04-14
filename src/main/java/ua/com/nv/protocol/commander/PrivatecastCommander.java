67845298adf5db16656b46c77be31f954505df58
Andriy Samsonenko <zigmund.raps@gmail.com>
2014-04-14 20:03:13 +0300

    s

--- src/main/java/ua/com/nv/protocol/commander/PrivatecastCommander.java
+++ src/main/java/ua/com/nv/protocol/commander/PrivatecastCommander.java
@@ -1,6 +1,9 @@
 package ua.com.nv.protocol.commander;
 
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class PrivatecastCommander extends AbstractCommander {
     public PrivatecastCommander() {
         this.concreteCommand = Commands.PRIVATE;
@@ -8,7 +11,8 @@ public class PrivatecastCommander extends AbstractCommander {
 
     @Override
     public void processRequest(String clientRequest) {
-        //To change body of implemented methods use File | Settings | File Templates.
+        String toWhomName="-[A-Z]+-";
+
     }
 
     @Override
@@ -24,5 +28,28 @@ public class PrivatecastCommander extends AbstractCommander {
     @Override
     public boolean isContinue() {
         return false;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+    private NameAndBody getClientCommandAndContent(String request) {
+        String regex ="-[A-Z]+-";//LOGOUT:, BROADCAST:, PRIVATE: and so on
+        Pattern pattern = Pattern.compile(regex);
+        Matcher m = pattern.matcher(request);
+        String command = "";
+        int end = 0;
+        if (m.find()) {
+            end = m.end();
+            command = request.substring(m.start(), end);
+        }
+        String content = request.substring(end);
+        return new NameAndBody(command, content);
+    }
+
+    private class NameAndBody {
+        String name;
+        String body;
+
+        NameAndBody(String name, String body) {
+            this.name = name;
+            this.body = body;
+        }
     }
 }
